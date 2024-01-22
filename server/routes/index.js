d51d83c577d86ace31c1080aa8e0df4664569aec
Sean B <seanb4@hotmail.co.uk>
2024-01-22 10:27:39 +0000

    front-end initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json( { content: 'home page' } )
+});
+
+router.get('/info', function(req, res, next) {
+  res.json( { content: 'info' } )
 });
 
 module.exports = router;
---
+++ server/routes/testing/index.test.js
@@ -0,0 +1,31 @@
+// Run with:
+// npx jest routes/testing/index.test.js
+
+const index = require("../index");
+
+const request = require("supertest");
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use("/", index);
+
+test("index route works", done => {
+  request(app)
+    .get("/")
+    .expect("Content-Type", /json/)
+    .expect( { content: "home page" } )
+    .expect(200, done);
+});
+
+// test("testing route works", done => {
+//   request(app)
+//     .post("/test")
+//     .type("form")
+//     .send({ item: "hey" })
+//     .then(() => {
+//       request(app)
+//         .get("/test")
+//         .expect({ array: ["hey"] }, done);
+//     });
+// });
